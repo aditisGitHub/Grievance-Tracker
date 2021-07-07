b5bb362ad4879812a8b83cad319e076071bb0863
Aaryan Dewan <aaryandewan@yahoo.com>
2021-07-07 12:51:43 +0530

    Add signup and signin pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from "react";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <Switch>
+        <Route exact path="/signin">
+          <Signin />
+        </Route>
 
-    </div>
+        <Route exact path="signup">
+          <Signup />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
