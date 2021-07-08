ead7de4c46ceb5efe80e012ced5b6f9773038a2c
Aaryan Dewan <aaryandewan@yahoo.com>
2021-07-08 17:09:05 +0530

    commit

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useHistory } from "react-router-dom";
+import { db } from "../firebase";
 
 export default function Profile() {
+  const [userData, setUserData] = useState("");
   const history = useHistory();
   const { currentUser, logout } = useAuth();
   async function logoutHandler() {
@@ -14,9 +16,47 @@ export default function Profile() {
     }
   }
 
+  useEffect(() => {
+    if (currentUser) {
+      var docRef = db.collection("users").doc(currentUser.uid);
+      docRef
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            console.log("Document data:", doc.data());
+            //   userData = doc.data();
+            setUserData(doc.data());
+            console.log("UserType", userData.userType);
+            //   console.log(typeof userData.userType);
+            console.log(userData.userType === "govt");
+          } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+          }
+        })
+        .catch((error) => {
+          console.log("Error getting document:", error);
+        });
+    }
+  }, []);
+
   return (
     <div>
+      {userData && userData.userType === "govt" ? (
+        <h1>GOVT COMPONENT</h1>
+      ) : (
+        <h2>SMCC Component</h2>
+      )}
+
+      {/* {if(userData.userType == "govt") {
+        return (<h1>GOVT</h1>)
+    }
+    else{
+        return(<h1>SMC</h1>)
+    }} */}
+
       <h1>{currentUser && currentUser.uid}</h1>
+      {<h1>{userData.userType}</h1>}
       <button onClick={logoutHandler}>Logout</button>
     </div>
   );
