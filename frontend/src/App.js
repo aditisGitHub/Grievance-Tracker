3c70e577ef5c154891ef6384530bb6e555539fe4
Aaryan Dewan <aaryandewan@yahoo.com>
2021-07-08 12:49:03 +0530

    dashboard path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Route exact path="/signin">
           <Signin />
         </Route>
-        <Route exact path="/">
+        <Route exact path="/dashboard">
           <Dashboard />
         </Route>
 
