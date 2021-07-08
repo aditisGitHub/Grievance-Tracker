a05c45e2dc34f73e69e2e4adf231a4057ea1ce4e
Aaryan Dewan <aaryandewan@yahoo.com>
2021-07-08 15:06:30 +0530

    Add signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,30 @@ import React from "react";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Dashboard from "./components/Dashboard/Dashboard";
+// import Dashboard from "./components/Dashboard/Dashboard";
+import { AuthProvider } from "./context/AuthContext";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/signin">
-          <Signin />
-        </Route>
-        <Route exact path="/dashboard">
+      <AuthProvider>
+        <Switch>
+          <Route exact path="/signin">
+            <Signin />
+          </Route>
+          {/* <Route exact path="/dashboard">
           <Dashboard />
-        </Route>
+        </Route> */}
+          <Route exact path="/">
+            <Profile />
+          </Route>
 
-        <Route exact path="/signup">
-          <Signup />
-        </Route>
-      </Switch>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </AuthProvider>
     </Router>
   );
 }
